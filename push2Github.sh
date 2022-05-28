ac935d803c56cf5654ebd198b91fd3fa736504dd
Frank <fdfanggang@163.com>
2022-05-28 20:36:55 +0800

    first commit

---
+++ push2Github.sh
@@ -0,0 +1,8 @@
+
+# push latest master to corresponding github repo.
+
+#git remote add githubRepo git@github.com:FrankKuiFang/commonCppPractice.git
+#git push githubRepo master
+
+git push git@github.com:FrankKuiFang/commonCppPractice.git master
+#@@@@
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+
+#include "hypod.hpp"
+
+
+int main(int argc, char** arg) {
+    std::cout << "Starting Main.....\n";
+    hypod();
+}
